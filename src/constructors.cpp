53a45c286d167dc5e1a5c660a460442fc3b4446e
Nikolay Khadzakos <hadzakos@gmail.com>
2024-02-13 23:44:05 +0300

    add -0 check

--- src/constructors.cpp
+++ src/constructors.cpp
@@ -9,23 +9,24 @@ BigNumber::BigNumber() {
 
 BigNumber::BigNumber (const std::string &str_number) {
     int i = 0;
+    int sgn = 0;
     if (str_number[0] == '-') {
         is_negative = true;
         i++;
+        sgn++;
     } else {
         is_negative = false;
     }
-
     for (; i < (int)str_number.size(); i++) {
         if (str_number[i] == '.') {
             break;
         }
     }
     
-    for (int j = i - 1; j >= 0; j--) {
+    for (int j = i - 1; j >= sgn; j--) {
         integer_part.push_back(static_cast<int>(str_number[j] - '0'));
     }
-    while (integer_part.size() && integer_part.back() == 0) {
+    while (integer_part.size() > 1 && integer_part.back() == 0) {
         integer_part.pop_back();
     }
 
@@ -37,9 +38,15 @@ BigNumber::BigNumber (const std::string &str_number) {
         real_part.pop_back();
     }
 
-
     real_size = real_part.size();
     integer_size = integer_part.size();
     number_size = integer_part.size() + real_part.size();
+
+    if (is_negative) {
+        if (is_zero()) {
+            std::cout << str_number << std::endl;
+            is_negative = false;
+        }
+    }
 }
 
