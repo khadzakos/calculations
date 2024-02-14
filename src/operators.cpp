771d2eb700f092707103e46162c7efed79f23200
Nikolay Khadzakos <hadzakos@gmail.com>
2024-02-14 16:31:56 +0300

    simplified operaror>

--- src/operators.cpp
+++ src/operators.cpp
@@ -1,3 +1,5 @@
+// #TODO DEBUG
+
 #include <long_arithmetic.h>
 
 const bool operator== (const BigNumber &a, const BigNumber &b) {
@@ -87,67 +89,14 @@ const bool operator< (const BigNumber &a, const BigNumber &b) {
     }
 }
 
-const bool operator> (const BigNumber &a, const BigNumber &b) {
-    if (!a.is_negative && b.is_negative) {
-        return true;
-    } else if (a.is_negative && !b.is_negative) {
-        return false;
-    } else {
-        if (!a.is_negative) {
-            if (a.integer_size > b.integer_size) {
-                return true;
-            } else if (a.integer_size < b.integer_size) {
-                return false;
-            } else {
-                int size = a.integer_size;
-                for (int i = size - 1; i >= 0; i--) {
-                    if (a.integer_part[i] > b.integer_part[i]) {
-                        return true;
-                    } else if (a.integer_part[i] < b.integer_part[i]) {
-                        return false;
-                    }
-                }
-                size = std::max(a.real_size, b.real_size);
-                for (int i = 0; i < size; i++) {
-                    if ((i < a.real_size ? a.real_part[i] : 0) > (i < b.real_size ? b.real_part[i] : 0)) {
-                        return true;
-                    } else if ((i < a.real_size ? a.real_part[i] : 0) < (i < b.real_size ? b.real_part[i] : 0)) {
-                        return false;
-                    }
-                }
-                return false;
-            }
-        } else {
-            if (a.integer_size < b.integer_size) {
-                return true;
-            } else if (a.integer_size > b.integer_size) {
-                return false;
-            } else {
-                int size = a.integer_size;
-                for (int i = size - 1; i >= 0; i--) {
-                    if (a.integer_part[i] <  b.integer_part[i]) {
-                        return true;
-                    } else if (a.integer_part[i] > b.integer_part[i]) {
-                        return false;
-                    }
-                }
-                size = std::max(a.real_size, b.real_size);
-                for (int i = 0; i < size; i++) {
-                    if ((i < a.real_size ? a.real_part[i] : 0) < (i < b.real_size ? b.real_part[i] : 0)) {
-                        return true;
-                    } else if ((i < a.real_size ? a.real_part[i] : 0) > (i < b.real_size ? b.real_part[i] : 0)) {
-                        return false;
-                    }
-                }
-                return false;
-            }
-        }
-    }
-}
-
 const bool operator<= (const BigNumber &a, const BigNumber &b) {
     return a < b || a == b;
 }
+
+const bool operator> (const BigNumber &a, const BigNumber &b) {
+    return !(a <= b);
+}
+
 
 const bool operator>= (const BigNumber &a, const BigNumber &b) {
     return a > b || a == b;
