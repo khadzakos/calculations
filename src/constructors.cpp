82595feb0ed8b2271455d11d6bac73292c4d1719
Nikolay Khadzakos <hadzakos@gmail.com>
2024-02-16 16:53:09 +0300

    changed constructor for new realization

--- src/constructors.cpp
+++ src/constructors.cpp
@@ -1,52 +1,33 @@
 #include <long_arithmetic.h>
 
+const int MAX_FRACTIONAL_SIZE = 250;
+
 BigNumber::BigNumber() {
     is_negative = true;
-    integer_size = 0;
-    real_size = 0;
     number_size = 0;
+    point_index = 0;
 }
 
-BigNumber::BigNumber (const std::string &str_number) {
-    int i = 0;
-    int sgn = 0;
-    if (str_number[0] == '-') {
-        is_negative = true;
-        i++;
-        sgn++;
-    } else {
-        is_negative = false;
-    }
-    for (; i < (int)str_number.size(); i++) {
-        if (str_number[i] == '.') {
-            break;
-        }
+BigNumber::BigNumber (const std::string &str) {
+    is_negative = str[0] == '-';
+    number = remove_leading_zeros(str.substr(is_negative));
+    reverse(number.begin(), number.end());
+    number = remove_leading_zeros(number.substr(0));
+    point_index = number.find('.');
+    number_size = number.size();
+
+    if (point_index == std::string::npos) {
+        point_index = number.size();
     }
     
-    for (int j = i - 1; j >= sgn; j--) {
-        integer_part.push_back(static_cast<int>(str_number[j] - '0'));
+    if (point_index == number_size - 1) {
+        number.push_back('0');
     }
-    while (integer_part.size() > 1 && integer_part.back() == 0) {
-        integer_part.pop_back();
-    }
+    number.erase(point_index, 1);
+    number_size = number.size();
 
-    for (i++; i < str_number.size(); i++) {
-        real_part.push_back(static_cast<int>(str_number[i] - '0'));
-    }
-
-    while (real_part.size() && real_part.back() == 0) {
-        real_part.pop_back();
-    }
-
-    real_size = real_part.size();
-    integer_size = integer_part.size();
-    number_size = integer_part.size() + real_part.size();
-
-    if (is_negative) {
-        if (is_zero()) {
-            std::cout << str_number << std::endl;
-            is_negative = false;
-        }
-    }
+    if (is_negative && is_zero()) {
+        is_negative = false;
+    } 
 }
 
