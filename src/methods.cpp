667a82071e9d283026b1acc06373ecb368311c7c
Nikolay Khadzakos <hadzakos@gmail.com>
2024-02-19 15:31:54 +0300

    changed remove_leading_zeros and to_string(now using copy)

--- src/methods.cpp
+++ src/methods.cpp
@@ -4,38 +4,34 @@ const bool BigNumber::is_zero () {
     return std::count(number.begin(), number.end(), '0') == number.size();
 }
 
-const std::string BigNumber::remove_leading_zeros(const std::string &str) {
-    int i = 0;
-    while (i < str.size() && str[i] == '0') {
-        if (str[i] == '.') {
-            break;
-        }
-        i++;
+void BigNumber::remove_leading_zeros() {
+    while (number.size() > point_index && number.back() == '0') {
+        number.pop_back();
     }
-    return str.substr(i);
 }
 
 const std::string BigNumber::to_string() {
     std::string str;
+    std::string copy_number = number;
 
     int i = 0;
-    while (i < point_index && number[i] == '0') {
+    while (i < point_index && copy_number[i] == '0') {
         i++;
     }
     point_index -= i;
-    number = number.substr(i);
+    copy_number = copy_number.substr(i);
 
     for (int i = 0; i < point_index; i++) {
-        str.push_back(number[i]);
+        str.push_back(copy_number[i]);
     }
     if (point_index > 0) {
         str.push_back('.');
     }
-    for (int i = point_index; i < number.size(); i++) {
-        str.push_back(number[i]);
+    for (int i = point_index; i < copy_number.size(); i++) {
+        str.push_back(copy_number[i]);
     }
     reverse(str.begin(), str.end());
-    if (is_negative) {
+    if (is_negative && !is_zero()) {
         str.insert(str.begin(), '-');
     }
     return str;
