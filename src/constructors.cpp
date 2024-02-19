02548fd4367fdae51feabc0321ce6915e003d876
Nikolay Khadzakos <hadzakos@gmail.com>
2024-02-19 15:33:16 +0300

    add flag to constructor

--- src/constructors.cpp
+++ src/constructors.cpp
@@ -5,37 +5,46 @@ BigNumber::BigNumber() {
     point_index = 0;
 }
 
-BigNumber::BigNumber (const std::string &str) {
+BigNumber::BigNumber (const std::string &str, bool flag) {
     is_negative = str[0] == '-';
-    number = remove_leading_zeros(str.substr(is_negative));
-    point_index = number.find('.');
-    if (point_index == std::string::npos) {
-        number.push_back('.');
-        point_index = number.size();
+
+    int i = is_negative;
+    while (i < str.size() && str[i] == '0') {
+        if (str[i] == '.') {
+            break;
+        }
+        i++;
     }
-    
-    int pos = point_index;
-    for (int cnt = 0; cnt < MAX_FRACTIONAL_SIZE; pos++, cnt++) {
-        if (pos == number.size()) {
-            number.push_back('0');
+    number = str.substr(i);
+
+    if (flag) {
+        point_index = number.find('.');
+        if (point_index == std::string::npos) {
+            number.push_back('.');
+            point_index = number.size();
+        }
+        
+        int pos = point_index;
+        for (int cnt = 0; cnt < MAX_FRACTIONAL_SIZE; pos++, cnt++) {
+            if (pos == number.size()) {
+                number.push_back('0');
+            }
+        }
+        while (number.size() > pos) {
+            number.pop_back();
         }
     }
-    while (number.size() > pos) {
-        number.pop_back();
-    }
-    
 
     reverse(number.begin(), number.end());
-    point_index = number.find('.');
-
-    if (point_index == std::string::npos) {
-        point_index = number.size();
+    if (flag) {
+        point_index = number.find('.');
+        if (point_index + 1 == number.size()) {
+            number.push_back('0');
+        }
+        number.erase(point_index, 1);
+    } else {
+        point_index = 0;
     }
-    
-    if (point_index + 1 == number.size()) {
-        number.push_back('0');
-    }
-    number.erase(point_index, 1);
 
     if (is_negative && is_zero()) {
         is_negative = false;
