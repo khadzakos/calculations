becc1b681f4452153c8b660d15ceeab9850af378
Nikolay Khadzakos <hadzakos@gmail.com>
2024-02-19 22:24:56 +0300

    add precision

--- src/methods.cpp
+++ src/methods.cpp
@@ -1,6 +1,6 @@
 #include <long_arithmetic.h>
 
-const bool BigNumber::is_zero () {
+const bool BigNumber::is_zero() {
     return std::count(number.begin(), number.end(), '0') == number.size();
 }
 
@@ -10,7 +10,7 @@ void BigNumber::remove_leading_zeros() {
     }
 }
 
-const std::string BigNumber::to_string() {
+const std::string BigNumber::to_string(const int &precision) {
     std::string str;
     std::string copy_number = number;
 
@@ -33,6 +33,22 @@ const std::string BigNumber::to_string() {
     reverse(str.begin(), str.end());
     if (is_negative && !is_zero()) {
         str.insert(str.begin(), '-');
+    }
+
+    if (precision > -1) {
+        int pos = str.find('.');
+        if (pos == std::string::npos) {
+            str.push_back('.');
+            pos = str.size();
+        }
+        for (int cnt = 0; cnt <= precision; pos++, cnt++) {
+            if (pos == str.size()) {
+                str.push_back('0');
+            }
+        }
+        while (str.size() > pos) {
+            str.pop_back();
+        }
     }
     return str;
 }
