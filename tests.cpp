3dbeb881212efd1bcc22f25ffd2be5f8f1f7177a
Nikolay Khadzakos <hadzakos@gmail.com>
2024-02-21 21:13:02 +0300

    add test show

--- tests.cpp
+++ tests.cpp
@@ -7,6 +7,9 @@ TEST(BigNumber, Constructor1) {
     BigNumber c;
 
     c = BigNumber("-0000.000");
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string() << " = " << "0" << std::endl;
+
     EXPECT_EQ(c.to_string(), "0");
 }
 
@@ -14,6 +17,9 @@ TEST(BigNumber, Constructor2) {
     BigNumber c;
 
     c = BigNumber("-000000123.00000010000000");
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string() << " = " << "-123.0000001" << std::endl;
+
     EXPECT_EQ(c.to_string(), "-123.0000001");
 }
 
@@ -21,6 +27,9 @@ TEST(BigNumber, Constructor3) {
     BigNumber c;
 
     c = BigNumber("123.1424240");
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string() << " = " << "123.142424" << std::endl;
+
     EXPECT_EQ(c.to_string(), "123.142424");
 }
 
@@ -33,6 +42,9 @@ TEST(BigNumber, Addition1) {
     a = BigNumber("-24.000010000000000");
     b = BigNumber("-0125.");
     BigNumber c = a + b;
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string() << " = " << "-149.00001" << std::endl;
+
     EXPECT_EQ(c.to_string(), "-149.00001");
 }
 
@@ -43,6 +55,9 @@ TEST(BigNumber, Addition2) {
     a = BigNumber("000123.99876");
     b = BigNumber("054.00124");
     BigNumber c = a + b;
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string() << " = " << "178" << std::endl;
+
     EXPECT_EQ(c.to_string(), "178");
 }
 
@@ -53,6 +68,9 @@ TEST(BigNumber, Addition3) {
     a = BigNumber("98.123");
     b = BigNumber("-99");
     BigNumber c = a + b;
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string() << " = " << "-0.877" << std::endl;
+
     EXPECT_EQ(c.to_string(), "-0.877");
 }
 
@@ -63,6 +81,9 @@ TEST(BigNumber, Addition4) {
     a = BigNumber("41");
     b = BigNumber("-5.1");
     BigNumber c = a + b;
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string() << " = " << "35.9" << std::endl;
+
     EXPECT_EQ(c.to_string(), "35.9");
 }
 
@@ -75,6 +96,9 @@ TEST(BigNumber, Subtraction1) {
     a = BigNumber("-293.00000000000000");
     b = BigNumber("-054.00124");
     BigNumber c = a - b;
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string() << " = " << "-238.99876" << std::endl;
+
     EXPECT_EQ(c.to_string(), "-238.99876");
 }
 
@@ -85,6 +109,9 @@ TEST(BigNumber, Subtraction2) {
     a = BigNumber("5");
     b = BigNumber("5.00124");
     BigNumber c = a - b;
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string() << " = " << "-0.00124" << std::endl;
+
     EXPECT_EQ(c.to_string(), "-0.00124");
 }
 
@@ -95,6 +122,9 @@ TEST(BigNumber, Subtraction3) {
     a = BigNumber("-17.3454");
     b = BigNumber("5");
     BigNumber c = a - b;
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string() << " = " << "-22.3454" << std::endl;
+
     EXPECT_EQ(c.to_string(), "-22.3454");
 }
 
@@ -105,6 +135,9 @@ TEST(BigNumber, Subtraction4) {
     a = BigNumber("26");
     b = BigNumber("-5.1");
     BigNumber c = a - b;
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string() << " = " << "31.1" << std::endl;
+
     EXPECT_EQ(c.to_string(), "31.1");
 }
 
@@ -117,6 +150,9 @@ TEST(BigNumber, Multiplication1) {
     a = BigNumber("1.00123");
     b = BigNumber("0.00123");
     BigNumber c = a * b;
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string() << " = " << "0.0012315129" << std::endl;
+
     EXPECT_EQ(c.to_string(), "0.0012315129");
 }
 
@@ -127,6 +163,9 @@ TEST(BigNumber, Multiplication2) {
     a = BigNumber("123.0000");
     b = BigNumber("-54.00124");
     BigNumber c = a * b;
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string() << " = " << "-6642.15252" << std::endl;
+
     EXPECT_EQ(c.to_string(), "-6642.15252");
 }
 
@@ -137,6 +176,9 @@ TEST(BigNumber, Multiplication3) {
     a = BigNumber("0.0000");
     b = BigNumber("0.00124");
     BigNumber c = a * b;
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string() << " = " << "0" << std::endl;
+
     EXPECT_EQ(c.to_string(), "0");
 }
 
@@ -147,6 +189,9 @@ TEST(BigNumber, Multiplication4) {
     a = BigNumber("1234567890");
     b = BigNumber("123456789123456789");
     BigNumber c = a * b;
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string() << " = " << "152415787654320997501905210" << std::endl;
+
     EXPECT_EQ(c.to_string(), "152415787654320997501905210");
 }
 
@@ -159,6 +204,9 @@ TEST(BigNumber, Division1) {
     a = BigNumber("1.0000123");
     b = BigNumber("0.000123");
     BigNumber c = a / b;
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string(9) << " = " << "8130.181300813" << std::endl;
+
     EXPECT_EQ(c.to_string(9), "8130.181300813");
 }
 
@@ -169,6 +217,9 @@ TEST(BigNumber, Division2) {
     a = BigNumber("112");
     b = BigNumber("-54.00124");
     BigNumber c = a / b;
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string(9) << " = " << "-2.074026448" << std::endl;
+
     EXPECT_EQ(c.to_string(9), "-2.074026448");
 }
 
@@ -179,6 +230,9 @@ TEST(BigNumber, Division3) {
     a = BigNumber("1");
     b = BigNumber("3");
     BigNumber c = a / b;
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string(9) << " = " << "0.333333333" << std::endl;
+
     EXPECT_EQ(c.to_string(9), "0.333333333");
 }
 
@@ -189,6 +243,9 @@ TEST(BigNumber, Division4) {
     a = BigNumber("754");
     b = BigNumber("0.00000645");
     BigNumber c = a / b;
+    BigNumber copy_c = c;
+    std::cout << copy_c.to_string(9) << " = " << "116899224.806201550" << std::endl;
+
     EXPECT_EQ(c.to_string(9), "116899224.806201550");
 }
 
@@ -200,6 +257,13 @@ TEST(BigNumber, Comparison1) {
 
     a = BigNumber("123.01124");
     b = BigNumber("123.01124");
+    std::cout << a.to_string() << " == " << b.to_string() << ": " << (a == b) << std::endl;
+    std::cout << a.to_string() << " != " << b.to_string() << ": " << (a != b) << std::endl;
+    std::cout << a.to_string() << " < " << b.to_string() << ": " << (a < b) << std::endl;
+    std::cout << a.to_string() << " > " << b.to_string() << ": " << (a > b) << std::endl;
+    std::cout << a.to_string() << " <= " << b.to_string() << ": " << (a <= b) << std::endl;
+    std::cout << a.to_string() << " >= " << b.to_string() << ": " << (a >= b) << std::endl;
+
     EXPECT_EQ(a == b, true);
     EXPECT_EQ(a != b, false);
     EXPECT_EQ(a < b, false);
@@ -214,6 +278,13 @@ TEST(BigNumber, Comparison2) {
 
     a = BigNumber("-100.01184");
     b = BigNumber("-100.01124");
+    std::cout << a.to_string() << " == " << b.to_string() << ": " << (a == b) << std::endl;
+    std::cout << a.to_string() << " != " << b.to_string() << ": " << (a != b) << std::endl;
+    std::cout << a.to_string() << " < " << b.to_string() << ": " << (a < b) << std::endl;
+    std::cout << a.to_string() << " > " << b.to_string() << ": " << (a > b) << std::endl;
+    std::cout << a.to_string() << " <= " << b.to_string() << ": " << (a <= b) << std::endl;
+    std::cout << a.to_string() << " >= " << b.to_string() << ": " << (a >= b) << std::endl;
+
     EXPECT_EQ(a == b, false);
     EXPECT_EQ(a != b, true);
     EXPECT_EQ(a < b, true);
